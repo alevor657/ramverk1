569f907350ebbe9b6e12a127904488bf77dcfce8
Alexey <alevor657@gmail.com>
2017-09-01 00:03:47 +0200

    More fixes...

--- view/common/navbar.php
+++ view/common/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="<?= $this->url('') ?>">Home</a>
+    <a class="navbar-brand" href="<?= $app->url->create('') ?>">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,10 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="<?= $this->url('/report') ?>">Report <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="<?= $app->url->create('report') ?>">Report <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="<?= $this->url('/about') ?>">About</a>
+                <a class="nav-link" href="<?= $app->url->create('about') ?>">About</a>
             </li>
             <!-- <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
